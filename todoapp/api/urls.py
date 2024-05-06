b0ec9ae54c4753d88df2576ac882d5ac2536011c
ParthipPR <parthip2512003@gmail.com>
2024-05-06 17:39:30 +0530

    "Fix:corrected errors in api"

--- todoapp/api/urls.py
+++ todoapp/api/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('updatepage/<int:task_id>/', views.updatepage, name='updatepage'),
-    path('tasks/', views.tasks, name='tasks'),
-    path('completed/<int:task_id>/', views.completed, name='completed'),
-    path('delete/<int:task_id>/', views.delete, name='delete'),
-    path('add_task/', views.add_task, name='add_task'),
-    path('update/<int:task_id>/', views.update, name='update'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('updatepage/<int:task_id>/', views.updatepage, name='api_updatepage'),
+    path('tasks/', views.tasks, name='api_tasks'),
+    path('completed/<int:task_id>/', views.completed, name='api_completed'),
+    path('delete/<int:task_id>/', views.delete, name='api_delete'),
+    path('add_task/', views.add_task, name='api_add_task'),
+    path('update/<int:task_id>/', views.update, name='api_update'),
+    path('register/', views.register, name='api_register'),
+    path('login/', views.login, name='api_login'),
+    path('logout/', views.logout, name='api_logout'),
 ]
